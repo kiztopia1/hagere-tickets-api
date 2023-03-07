70cd5250461cf083cb562ac5dbb9b47443b7514d
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-03-07 17:36:54 +0300

    event controller added

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
-
+use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes'
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get("/events", [EventController::class, 'index']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
